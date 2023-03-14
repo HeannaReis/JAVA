e1176396c0671dd8767fc288201f07016a99f1a6
HeannaReis <heannareis@gmail.com>
2023-03-14 01:25:52 -0300

    :construction: feat(methods): Study of Methods

--- lab-banco-digital-oo/src/ContaPoupanca.java
+++ lab-banco-digital-oo/src/ContaPoupanca.java
@@ -7,7 +7,7 @@ public class ContaPoupanca extends Conta {
 
 	@Override
 	public void imprimirExtrato() {
-		System.out.println("=== Extrato Conta Poupança ===");
+		System.out.println("=== Extrato Conta PoupanÃ§a ===");
 		super.imprimirInfosComuns();
 	}
 }
